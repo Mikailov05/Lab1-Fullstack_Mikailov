a6fd8b56912c420c315aec12fffa9556d550d3ef
axmed <mikailov_akhmed@bk.ru>
2026-01-22 20:53:28 +0300

    Created terminal advanced files

--- program.cs
+++ program.cs
@@ -1,5 +1,19 @@
 using System;
 
-Console.WriteLine("Ассаламу алейкум!");
-Console.WriteLine("Микаилов Ахмед");
-Console.WriteLine($"22.01.26: {DateTime.Now}");
+Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Привет, {name}!");
+DayOfWeek today = DateTime.Now.DayOfWeek;
+Console.WriteLine($"Сегодня: {today}")
+Console.Write("Хотите продолжить? (y/n): ");
+string answer = Console.ReadLine();
+
+if (answer == "y")
+{
+    Console.WriteLine("Продолжаем работу...");
+}
+else
+{
+    Console.WriteLine("Завершение программы.");
+}
